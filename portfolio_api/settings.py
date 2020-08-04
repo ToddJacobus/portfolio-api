223815fcc65d2970801a7b7f513621db3cd678f1
Todd Jacobus <rtjacobus1@gmail.com>
2020-08-04 13:29:34 -1000

    added procfile; django-heroku; production settings

--- portfolio_api/settings.py
+++ portfolio_api/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -19,7 +20,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = True if os.environ.get('DEBUG').lower() == 'true' else False
@@ -122,3 +122,25 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# PRODUCTION SETTINGS
+
+# Throttling
+REST_FRAMEWORK = {
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '100/day',
+        'user': '1000/day'
+    }
+}
+
+# Don't send tokens and keys over http
+CSRF_COOKIE_SECURE = True if not os.environ.get('DEBUG').lower() == 'true' else False
+SESSION_COOKIE_SECURE = True if not os.environ.get('DEBUG').lower() == 'true' else False
+
+
+# Configure Django App for Heroku.
+django_heroku.settings(locals())
