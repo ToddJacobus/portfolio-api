add239dacefbcb9ad8e95d616fc616a9f6e678ac
Todd Jacobus <rtjacobus1@gmail.com>
2020-08-05 10:42:20 -1000

    configured a STATIC_ROOT

--- portfolio_api/settings.py
+++ portfolio_api/settings.py
@@ -134,6 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/staticfiles/'
 
 # Custom user model
 AUTH_USER_MODEL = 'core.User'
