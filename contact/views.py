0ccaba9843f7c66db38bea458b90f3c6529e2fb3
Todd Jacobus <rtjacobus1@gmail.com>
2020-08-05 11:23:16 -1000

    swapped viewset out for modelviewset to see if this fixes the issue in deployment

--- contact/views.py
+++ contact/views.py
@@ -4,12 +4,15 @@ from core.models import Contact
 from contact import serializers
 
 
-class ContactVewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
-    """A simple write-only viewset"""
+# class ContactVewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
+#     """A simple write-only viewset"""
+#     serializer_class = serializers.ContactSerializer
+#     queryset = Contact.objects.all()
+
+#     def perform_create(self, serializer):
+#         """create a new contact entry"""
+#         serializer.save()
+
+class ContactVewSet(viewsets.ModelViewSet):
     serializer_class = serializers.ContactSerializer
     queryset = Contact.objects.all()
-
-    def perform_create(self, serializer):
-        """create a new contact entry"""
-        serializer.save()
-        
